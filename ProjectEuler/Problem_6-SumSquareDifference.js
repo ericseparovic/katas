7931c1d3d58edb579de6b797b1de7e51fd455c17
Eric Separovic <ericseparovic07@gmail.com>
2022-10-19 07:25:11 -0300

    Add solution problem 6

--- ProjectEuler/Problem_6-SumSquareDifference.js
+++ ProjectEuler/Problem_6-SumSquareDifference.js
@@ -5,3 +5,30 @@
 // Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
 
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+const calcSumSquare = () => {
+
+    // The sum of the squares of the first ten natural numbers
+    let sumSquare = 0;
+
+
+    // The square of the sum of the first ten natural numbers
+    let sumFirstNatural = 0;
+
+    // The square of the sum of the first ten natural numbers
+    let squareSum = 0;
+
+    for (let i = 1; i <= 100; i++) {
+        sumSquare += i ** 2
+        sumFirstNatural += i
+
+        squareSum = sumFirstNatural ** 2
+    }
+
+    return console.log(squareSum - sumSquare);
+
+
+}
+
+
+calcSumSquare()
